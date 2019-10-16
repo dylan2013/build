12bf0c4ce61228693a9b920abbed5bcdebe99608
黃俊威 <dylan.huang@ischool.com.tw>
2019-10-16 11:15:16 +0800

    新增電子報表模組

---
+++ angular/src/assets/b3271aa2-f9d7-43b5-bcae-a36e058fda07_Dylan/js/ereport.js
@@ -0,0 +1,62 @@
+gadget.autofit(document.getElementById("widget"));
+				
+var _connection = gadget.getContract("OneAdmin.eReport.Student");
+var _dl_conn = gadget.getContract("OneAdmin.eReport.Public");
+// var _session_id;
+
+$(function() {
+	// _connection.send({
+	// 	service: "DS.Base.Connect",
+	// 	body: { RequestSessionID : "" },
+	// 	result: function(response, error, http) {
+	// 		_session_id = response.SessionID;
+	// 	}
+	// });
+
+	// 下載個人電子報表
+	_connection.send({
+		service: "GetReportList",
+		body: {
+			Content : { ID: "", Name: "", Format: "", Timestamp: "" }
+		},
+		result: function(response, error, http) {
+		
+			var thead =
+				"<tr>" +
+					//"<td class='ereport-header'>編號</td>" +
+					"<td class='ereport-header'>日期</td>" +
+					"<td class='ereport-header'>名稱</td>" +
+					"<td class='ereport-header'>格式</td>" +
+					"<td class='ereport-header'>下載</td>" +
+				"</tr>", tbody = "";
+			var items = [];
+			
+			if (response.Reports != null) {
+				$(response.Reports.Report).each(function() {
+					var tr = "";
+					//tr += "<td class='ereport-item'>" + this.ID + "</td>";
+					tr += "<td class='ereport-item'>" + this.Timestamp.substr(0, 16) + "</td>";
+					tr += "<td class='ereport-item'>" + (this.Name == "" ? "&nbsp;" : this.Name) + "</td>";
+					tr += "<td class='ereport-item'>" + (this.Format == "" ? "&nbsp;" : this.Format) + "</td>";
+					tr += "<td class='ereport-item'><a href='#' onclick='downloadPaper(\"" + this.ID + "\")'>下載</a></td>";
+					
+					tbody += "<tr>" + tr + "</tr>";
+				});
+			}
+			
+			$("#ereport-table thead").html(thead);
+			$("#ereport-table tbody").html(tbody);
+		}
+	});
+});
+
+
+function downloadPaper(paper_id) {
+
+	var url =
+		_dl_conn.getAccessPoint() +
+		"/Download?" +
+		"&content=<ID>" + paper_id + "</ID>";
+		
+	window.open(url, "_blank");
+}
---
+++ public/assets/7a29929b-6978-4ebc-be04-5eb4b8aa1fa7/js/ereport.js
@@ -0,0 +1,65 @@
+gadget.autofit(document.getElementById("widget"));
+				
+var _connection = gadget.getContract("OneAdmin.eReport.Teacher");
+var _dl_conn = gadget.getContract("OneAdmin.eReport.Public");
+var _papers = {};
+
+$(function() {
+
+	// 下載個人電子報表
+	_connection.send({
+		service: "GetReportList",
+		body: {
+			Content : { ID: "", Name: "", Format: "", Timestamp: "" }
+		},
+		result: function(response, error, http) {
+			
+			_papers.personal = [];
+			
+			if (response.Reports != null) {
+				$(response.Reports.Report).each(function() {
+					var tr = "";
+					tr += "<td class='ereport-item'>" + this.Viewer + "</td>";
+					tr += "<td class='ereport-item'>" + this.Timestamp.substr(0, 16) + "</td>";
+					tr += "<td class='ereport-item'>" + (this.Name == "" ? "&nbsp;" : this.Name) + "</td>";
+					tr += "<td class='ereport-item'>" + (this.Format == "" ? "&nbsp;" : this.Format) + "</td>";
+					tr += "<td class='ereport-item'><a href='#' onclick='downloadPaper(\"" + this.ID + "\")'>下載</a></td>";
+					
+					_papers.personal.push("<tr>" + tr + "</tr>");
+				});
+			}
+				
+			bind();
+		}
+	});
+});
+
+function bind() {
+	
+	var thead =
+		"<tr>" +
+			"<td class='ereport-header'>傳送給</td>" +
+			"<td class='ereport-header'>日期</td>" +
+			"<td class='ereport-header'>名稱</td>" +
+			"<td class='ereport-header'>格式</td>" +
+			"<td class='ereport-header'>下載</td>" +
+		"</tr>", tbody = "";
+	var items = [];
+	
+	$(_papers.personal).each(function() {
+		tbody += this;
+	});
+	
+	$("#ereport-table thead").html(thead);
+	$("#ereport-table tbody").html(tbody);
+}
+
+function downloadPaper(paper_id) {
+
+	var url =
+		_dl_conn.getAccessPoint() +
+		"/Download?" +
+		"&content=<ID>" + paper_id + "</ID>";
+		
+	window.open(url, "_blank");
+}
